af4fae0e7b0390d664956c806d75140e5e9d19f3
Artyom <worldbeater-dev@yandex.ru>
2022-03-28 00:06:28 +0300

    feature: Add tests for the new API endpoint

--- tests/api/test_groups.py
+++ tests/api/test_groups.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from tests.utils import unique_id
+from tests.utils import unique_str
 
 from flask.testing import FlaskClient
 
@@ -10,9 +10,13 @@ def test_group_prefixes(session: Session, client: FlaskClient):
     prefix_one = "prefix_one-"
     prefix_two = "prefix_two-"
     group_manager = GroupRepository(session)
-    group_manager.create_by_names([prefix_one + unique_id(), prefix_two + unique_id()])
+    group_manager.create_by_names([
+        prefix_one + unique_str(),
+        prefix_two + unique_str()
+    ])
 
     response = client.get("/api/v1/group/prefixes")
+
     assert response.is_json
     assert response.json["prefixes"] is not None
     assert "prefix_one" in response.json["prefixes"]
