18941af5bf284fc2b6afd28cd762080727b08f59
Artyom <worldbeater-dev@yandex.ru>
2022-03-28 00:33:43 +0300

    chore: Add unauthorized submission test

--- tests/api/test_submission.py
+++ tests/api/test_submission.py
@@ -30,3 +30,23 @@ def test_solution_submission(session: Session, client: FlaskClient):
     assert response.json["status"] == TaskStatusEnum.Submitted
     assert response.json["status_name"] == TaskStatusEnum.Submitted.name
     assert response.json["error_message"] is None
+
+
+def test_unauthorized_submission(session: Session, client: FlaskClient):
+    variant = unique_int()
+    task = unique_int()
+
+    db = AppDbContext(session)
+    group = db.groups.create(unique_str()).id
+    db.variants.create_by_ids([variant])
+    db.tasks.create_by_ids([task])
+
+    response = client.post(
+        f"/api/v1/group/{group}/variant/{variant}/task/{task}",
+        data=json.dumps(dict(code="main = lambda x: 42")),
+        headers=dict(token="INVALID_TOKEN"),
+        content_type='application/json'
+    )
+
+    assert response.json is not None
+    assert response.json["error"] == 500
