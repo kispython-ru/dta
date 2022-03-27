c07da8c2fed1fa0fa941ab1e779f41e07f7135ef
Artyom <worldbeater-dev@yandex.ru>
2022-03-27 22:57:34 +0300

    feature: Ability to POST solutions in the JSON format

--- webapp/api.py
+++ webapp/api.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
+from flask import current_app as app
+from flask import jsonify, request
 
+from webapp.forms import CodeLength, MessageFormParameters
 from webapp.managers import find_task_status
 from webapp.models import TaskStatus, TaskStatusEnum
 from webapp.repositories import AppDbContext
-from webapp.utils import handle_api_errors, use_db
+from webapp.utils import get_real_ip, handle_api_errors, use_db
 
 
 blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
@@ -74,6 +77,37 @@ def task(db: AppDbContext, gid: int, vid: int, tid: int):
     group = db.groups.get_by_id(gid)
     task = db.tasks.get_by_id(tid)
     ts = db.statuses.get_task_status(task.id, variant.id, group.id)
+    error_message = ts.output if ts is not None else ""
+    status = ts.status if ts is not None else TaskStatusEnum.NotSubmitted
+    path = "http://sovietov.com/kispython"
+    return jsonify({
+        "id": task.id,
+        "source": f"{path}/{task.id}/{group.title}.html#вариант-{variant.id}",
+        "status": status.value,
+        "status_name": status.name,
+        "error_message": error_message,
+    })
+
+
+@blueprint.route("/group/<gid>/variant/<vid>/task/<tid>", methods=["POST"])
+@handle_api_errors()
+@use_db()
+def submit_task(db: AppDbContext, gid: int, vid: int, tid: int):
+    if app.config["API_TOKEN"] != request.authorization.get("Basic"):
+        raise ValueError("Access is denied.")
+
+    json = request.get_json()
+    code = json["code"]
+    if not CodeLength.min < len(code) < CodeLength.max:
+        return ValueError("Bad request.")
+
+    variant = db.variants.get_by_id(vid)
+    group = db.groups.get_by_id(gid)
+    task = db.tasks.get_by_id(tid)
+    ip = get_real_ip(request)
+
+    db.messages.submit_task(task.id, variant.id, group.id, code, ip)
+    ts = db.statuses.submit_task(task.id, variant.id, group.id, code)
     error_message = ts.output if ts is not None else ""
     status = ts.status if ts is not None else TaskStatusEnum.NotSubmitted
     path = "http://sovietov.com/kispython"
