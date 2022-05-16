47036a97cf4d918c0e319e074271d46136293f88
zxc3buttons <tokare2002@gmail.com>
2022-05-16 19:17:44 +0300

    choosing csv delimiter drop-down list is added

--- webapp/views.py
+++ webapp/views.py
@@ -137,11 +137,12 @@ def exam(group_id: int, token: str):
     return render_template("exam.jinja", group=group, seed=seed, token=token)
 
 
-@blueprint.route("/exams/<token>/<group_id>/<s>/score_csv", methods=["POST"])
+@blueprint.route("/exams/<token>/<group_id>/score_csv", methods=["POST"])
 @require_final_token()
-def score_csv(group_id: int, token: str, s: str):
+def score_csv(group_id: int, token: str):
     variants = db.variants.get_all()
-    value = exports.export_exam_results(group_id, statuses, variants, s)
+    delimiter = request.form.get('delimiter')
+    value = exports.export_exam_results(group_id, statuses, variants, delimiter)
     output = make_response(value)
     output.headers["Content-Disposition"] = "attachment; filename=results.csv"
     output.headers["Content-type"] = "text/csv"
