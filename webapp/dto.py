df5e7a3c431d68e1be756ab494ba5b2c0a52b522
MinusD <lukovnikov50@mail.ru>
2023-02-24 21:33:41 +0300

    Fixed titles Fixed committed files

--- webapp/dto.py
+++ webapp/dto.py
@@ -21,6 +21,7 @@ class AppConfig:
         self.lks_oauth_client_id: str = config["LKS_OAUTH_CLIENT_ID"]
         self.lks_oauth_client_secret: str = config["LKS_OAUTH_CLIENT_SECRET"]
         self.lks_redirect_url: str = config["LKS_REDIRECT_URL"]
+        self.places_in_rating: int = config["PLACES_IN_RATING"]
 
     @property
     def exam(self) -> bool:
@@ -210,14 +211,17 @@ class GroupDto:
         self.variants = variants
 
 
-class GroupInRatingDto:
+class StudentInRatingDto:
     def __init__(
         self,
         group: Group,
+        variant: int,
         earned: int
     ):
         self.group = group
+        self.variant = variant
         self.earned = earned
+
 
 
 class SubmissionDto:
