7f5d9c93542dfaee3f3a2036b7587bd81f6333f0
Yuriy <yura582005@gmail.com>
2025-04-04 14:24:35 +0300

    fix: removed empty lines in ui/test_groups

--- tests/ui/test_groups.py
+++ tests/ui/test_groups.py
@@ -39,12 +39,10 @@ def test_group_html_link(db: AppDatabase, client: FlaskClient):
     html_dashboard = response.get_data(as_text=True)
 
     group_id = next(group.id for group in db.groups.get_by_prefix(prefix))
-
     response = client.get(f'/group/{group_id}')
     html_group = response.get_data(as_text=True)
 
     tags = [tag.get('href') for tag in get_tags(html_dashboard, 'a', True)]
-
     tag_contents = next(tag for tag in tags if tag == f'/group/{group_id}')
 
     response = client.get(tag_contents)
