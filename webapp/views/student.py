77280c13ec4ed6fae65f08a736377b5ab906bfa3
Yuralexer <yura582005@gmail.com>
2025-04-06 17:47:57 +0300

    fix: Optimized code expression of getting variant

--- webapp/views/student.py
+++ webapp/views/student.py
@@ -136,7 +136,7 @@ def home(student: Student | None):
         group_rating=config.config.groups,
         exam=config.config.exam,
         group=group,
-        variant=list(v for v in group.variants if v.id == student.variant)[0],
+        variant=next((v for v in group.variants if v.id == student.variant), None),
         variants=db.variants.get_all(),
         number_of_tasks=len(group.variants),
         tasks_statuses=tasks_statuses,
