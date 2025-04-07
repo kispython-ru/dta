036f629b3a31603196e249b3fe14ff77a7a3d328
Yuriy <yura582005@gmail.com>
2025-04-07 09:45:47 +0300

    fix: optimized HomeManager

--- webapp/managers.py
+++ webapp/managers.py
@@ -333,15 +333,13 @@ class HomeManager:
 
     def get_group_place(self, gid: int) -> int:
         groupings = self.statuses.get_group_rating()
-        for place in groupings.keys():
-            groups_in_place = groupings[place]
+        for place, groups_in_place in groupings.items():
             if gid in list(group_in_place.group.id for group_in_place in groups_in_place):
                 return place
 
     def get_student_place(self, gid: int, vid: int) -> int:
         student_rating = self.statuses.get_rating()
-        for place in student_rating.keys():
-            students_in_place = student_rating[place]
+        for place, students_in_place in student_rating:
             for student_in_place in students_in_place:
                 if student_in_place.group == gid and student_in_place.variant == vid:
                     return place
